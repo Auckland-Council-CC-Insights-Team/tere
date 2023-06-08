4cf969e2d9183abe30916f20e38d8fc0befbc534
mkacwork <mk.ting@aucklandcouncil.ac.nz>
2023-06-09 08:25:11 +1200

    read_excel_file accepts .xlsx by default

--- R/get_started.R
+++ R/get_started.R
@@ -10,7 +10,7 @@
 #' @return A tibble
 #' @export
 #'
-get_excel_file <- function(filename, sheetname = 1, skip_rows = 0, path = get_file_storage_path("MY_SHAREPOINT_FILES"), file_extension = c(".xlsx", ".xls")) {
+get_excel_file <- function(filename, sheetname = 1, skip_rows = 0, path = get_file_storage_path("MY_SHAREPOINT_FILES"), file_extension = ".xlsx") {
   readxl::read_excel(
     path = paste0(path, "/", filename, file_extension),
     sheet = sheetname,
